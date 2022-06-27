d209414b01f5d4b4b73d18c9bda2a6a5daac8249
Herisson Silva <herisson.dev@gmail.com>
2022-06-27 19:06:48 -0300

    Ajustando firebase.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
+import { useState } from "react";
+import React from 'react'
 import firebase from "./firebaseConnection";
-
+import "./style.css"
 function App() {
+
+  const [titulo, setTitulo] = useState('');
+  const [autor, setAutor] = useState('');
+
+  async function handleAdd() {
+    
+    await firebase.firestore().collection('posts')
+    .doc('12345')
+    .set({
+      titulo:titulo,
+      autor:autor
+    })
+    .then(()=>{
+      console.log('dados cadastrados com sucesso!');
+    })
+    .catch((error)=>{
+      console.log('houve um erro.');
+    })
+
+  }
+
   return (
     <div className="App">
       <h1>ReachJS + Firebase ;-)</h1>
+      <div className="container">
+        <label>Titulo: </label>
+        <textarea type="text" value={titulo} onChange={(e) => setTitulo(e.target.value)} />
+
+        <label>autor: </label>
+        <input type="text" value={autor} onChange={(e) => setAutor(e.target.value)} />
+        <button onClick={handleAdd}>Cadastrar</button>
       </div>
+    </div>
   );
 }
 
--- src/firebaseConnection.js
+++ src/firebaseConnection.js
@@ -1,20 +1,19 @@
 import firebase from "firebase/app";
-import initializeApp from "firebase/app";
 import 'firebase/firestore';
+import 'firebase/database';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDW2xlLNC1JiqpfduHMaRsUk7iRxTbTBU0",
-    authDomain: "cursoreact-7451b.firebaseapp.com",
-    projectId: "cursoreact-7451b",
-    storageBucket: "cursoreact-7451b.appspot.com",
-    messagingSenderId: "916104695375",
-    appId: "1:916104695375:web:cfc9dbb640ec8a54ad243a",
-    measurementId: "G-XCMPZM8J6D"
-  };
-  
-  // Initialize Firebase
-  if(firebase.apps.length){
-  const app = initializeApp(firebaseConfig);
-  }
+let firebaseConfig = {
+  apiKey: "AIzaSyDW2xlLNC1JiqpfduHMaRsUk7iRxTbTBU0",
+  authDomain: "cursoreact-7451b.firebaseapp.com",
+  projectId: "cursoreact-7451b",
+  storageBucket: "cursoreact-7451b.appspot.com",
+  messagingSenderId: "916104695375",
+  appId: "1:916104695375:web:cfc9dbb640ec8a54ad243a",
+  measurementId: "G-XCMPZM8J6D"
+};
 
-  export default firebase;
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+export default firebase;
